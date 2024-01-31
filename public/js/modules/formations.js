e3460062448b728f2c176625b6069ed4204525a8
Steeven Salgado <salgadosteeven@gmail.com>
2024-01-31 14:41:50 +0100

    link de tout les js

--- public/js/modules/formations.js
+++ public/js/modules/formations.js
@@ -2,6 +2,47 @@ import DATA from "./../../json/molengeek.json" assert { type: "json" };
 console.log(DATA.reseauxSociaux);
 
 
+//! ********************** Menu Burger ***************************
+let sidebar = document.querySelector(".sidebar");
+let menuButton = document.querySelector(".menuButton");
+let menuFermer = document.querySelector(".menuFermer");
+
+function showSidebar(event) {
+  event.preventDefault();
+  sidebar.style.display = "flex";
+  sidebar.style.padding = "0";
+  setTimeout(() => {
+    sidebar.style.width = "250px";
+  }, 10);
+}
+menuButton.addEventListener("click", showSidebar);
+
+function hideSidebar() {
+    sidebar.style.width = "0px";
+  setTimeout(() => {
+    
+    sidebar.style.display = "none"
+  }, 1200);
+ 
+}
+menuFermer.addEventListener("click", hideSidebar);
+//! *********************** Fin Menu Burger ************************
+
+//? ----------------------------------------------------------------
+
+//! ************************ DropDown Langue ***********************
+
+document.querySelector(".dropdown").addEventListener("focusin", () =>{
+  document.querySelector(".dropdown-contenue").style.display ="flex"
+
+})
+document.querySelector(".dropdown").addEventListener("focusout", () =>{
+    document.querySelector(".dropdown-contenue").style.display ="none"
+})
+
+//! *********************** Fin DropDown ***************************
+
+
 let liResaux = document.querySelector(".resauxSoc").querySelectorAll("a")
 
 Object.values(DATA.reseauxSociaux).forEach((element,index) => {
