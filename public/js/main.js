199c57de3e881ecad4c6f001e3da213686cc920e
Steeven Salgado <salgadosteeven@gmail.com>
2024-02-01 23:34:45 +0100

    fin section a propos

--- public/js/main.js
+++ public/js/main.js
@@ -18,12 +18,10 @@ function showSidebar(event) {
 menuButton.addEventListener("click", showSidebar);
 
 function hideSidebar() {
-    sidebar.style.width = "0px";
+  sidebar.style.width = "0px";
   setTimeout(() => {
-    
-    sidebar.style.display = "none"
+    sidebar.style.display = "none";
   }, 1200);
- 
 }
 menuFermer.addEventListener("click", hideSidebar);
 //! *********************** Fin Menu Burger ************************
@@ -32,13 +30,12 @@ menuFermer.addEventListener("click", hideSidebar);
 
 //! ************************ DropDown Langue ***********************
 
-document.querySelector(".dropdown").addEventListener("focusin", () =>{
-  document.querySelector(".dropdown-contenue").style.display ="flex"
-
-})
-document.querySelector(".dropdown").addEventListener("focusout", () =>{
-    document.querySelector(".dropdown-contenue").style.display ="none"
-})
+document.querySelector(".dropdown").addEventListener("focusin", () => {
+  document.querySelector(".dropdown-contenue").style.display = "flex";
+});
+document.querySelector(".dropdown").addEventListener("focusout", () => {
+  document.querySelector(".dropdown-contenue").style.display = "none";
+});
 
 //! *********************** Fin DropDown ***************************
 
@@ -46,51 +43,43 @@ document.querySelector(".dropdown").addEventListener("focusout", () =>{
 
 //! ********************** Section 2 Partenaires Home *******************
 
-		let slides = document.querySelectorAll(".item");
-		let button = document.querySelectorAll(".button");
+let slides = document.querySelectorAll(".item");
+let buttons = document.querySelectorAll(".button");
 
-		let current = 0;
-		let prev = 7;
-		let next = 1;
+let current = 0;
+let prev = slides.length - 1;
+let next = 1;
 
-		for (let i = 0; i < button.length; i++) {
-			button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
-		}
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", () => (i == 0 ? gotoPrev() : gotoNext()));
+}
 
-		let gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+let gotoPrev = () =>
+  current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
 
-		let gotoNext = () => current < 7 ? gotoNum(current + 1) : gotoNum(0);
+let gotoNext = () => (current < slides.length - 1 ? gotoNum(current + 1) : gotoNum(0));
 
-		let gotoNum = number => {
-			current = number;
-			prev = current - 1;
-			next = current + 1;
+let gotoNum = (number) => {
+  current = number;
+  prev = current - 1 < 0 ? slides.length - 1 : current - 1;
+  next = current + 1 >= slides.length ? 0 : current + 1;
 
-			for (let i = 0; i < slides.length; i++) {
-				slides[i].classList.remove("active");
-				slides[i].classList.remove("prev");
-				slides[i].classList.remove("next");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+    slides[i].classList.remove("prev");
+    slides[i].classList.remove("next");
+  }
 
-			}
+  slides[current].classList.add("active");
+  slides[prev].classList.add("prev");
+  slides[next].classList.add("next");
+};
 
-			if (next == 8) {
-				next = 0;
-			}
 
-			if (prev == -1) {
-				prev = 7;
-			}
-
-			slides[current].classList.add("active");
-			slides[prev].classList.add("prev");
-			slides[next].classList.add("next");
-		}
-
-    let imageSection2 = document.querySelector(".items").querySelectorAll("img")
-    Object.values(DATA.partenaires).forEach((element, index) => {
-      imageSection2[index].src = `${element.logo}`
-    });
-
+let imageSection2 = document.querySelector(".items").querySelectorAll("img");
+Object.values(DATA.partenaires).forEach((element, index) => {
+  imageSection2[index].src = `${element.logo}`;
+});
 
 //! ********************* Fin Section Partenaire ********************
 
@@ -98,18 +87,18 @@ document.querySelector(".dropdown").addEventListener("focusout", () =>{
 
 //! ***************************** Contact Home****************************
 
-let liResaux = document.querySelector(".resauxSoc").querySelectorAll("a")
+let liResaux = document.querySelector(".resauxSoc").querySelectorAll("a");
 
-Object.values(DATA.reseauxSociaux).forEach((element,index) => {
-	liResaux[index].href = element
-	console.log(element);
-})
+Object.values(DATA.reseauxSociaux).forEach((element, index) => {
+  liResaux[index].href = element;
+  console.log(element);
+});
 
-let contactP = document.querySelector(".bull").querySelectorAll("p")
+let contactP = document.querySelector(".bull").querySelectorAll("p");
 
-Object.values(DATA.molengeekInfo).forEach((element,index) => {
-	contactP[index].innerText += ` ${element}`
-})
+Object.values(DATA.molengeekInfo).forEach((element, index) => {
+  contactP[index].innerText += ` ${element}`;
+});
 
 //! ************************* Fin Contact ************************
 
