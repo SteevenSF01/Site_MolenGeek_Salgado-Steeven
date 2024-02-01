8b724402bd7cf23f812fee70dc22e00cb299b71b
Steeven Salgado <salgadosteeven@gmail.com>
2024-02-01 14:39:11 +0100

    Fin Formations Navigation + responsive

--- public/js/modules/formations.js
+++ public/js/modules/formations.js
@@ -1,7 +1,6 @@
 import DATA from "./../../json/molengeek.json" assert { type: "json" };
 // console.log(DATA.reseauxSociaux);
 
-
 //! ********************** Menu Burger ***************************
 let sidebar = document.querySelector(".sidebar");
 let menuButton = document.querySelector(".menuButton");
@@ -18,12 +17,10 @@ function showSidebar(event) {
 menuButton.addEventListener("click", showSidebar);
 
 function hideSidebar() {
-    sidebar.style.width = "0px";
+  sidebar.style.width = "0px";
   setTimeout(() => {
-    
-    sidebar.style.display = "none"
+    sidebar.style.display = "none";
   }, 1200);
- 
 }
 menuFermer.addEventListener("click", hideSidebar);
 //! *********************** Fin Menu Burger ************************
@@ -32,72 +29,110 @@ menuFermer.addEventListener("click", hideSidebar);
 
 //! ************************ DropDown Langue ***********************
 
-document.querySelector(".dropdown").addEventListener("focusin", () =>{
-  document.querySelector(".dropdown-contenue").style.display ="flex"
-
-})
-document.querySelector(".dropdown").addEventListener("focusout", () =>{
-    document.querySelector(".dropdown-contenue").style.display ="none"
-})
+document.querySelector(".dropdown").addEventListener("focusin", () => {
+  document.querySelector(".dropdown-contenue").style.display = "flex";
+});
+document.querySelector(".dropdown").addEventListener("focusout", () => {
+  document.querySelector(".dropdown-contenue").style.display = "none";
+});
 
 //! *********************** Fin DropDown ***************************
 
+let liResaux = document.querySelector(".resauxSoc").querySelectorAll("a");
 
-let liResaux = document.querySelector(".resauxSoc").querySelectorAll("a")
-
-Object.values(DATA.reseauxSociaux).forEach((element,index) => {
-	liResaux[index].href = element
-})
+Object.values(DATA.reseauxSociaux).forEach((element, index) => {
+  liResaux[index].href = element;
+});
 
 //! ************************** Acrodeon ***************************
+
+let bntLongues = document.querySelector(".btnfl")
+let bntCourtes = document.querySelector(".btnfc")
+
+bntLongues.addEventListener("click",()=>{
+    document.querySelector(".formationsLongues").style.display ="block"
+    document.querySelector(".formationsCourtes").style.display = "none"    
+})
+bntCourtes.addEventListener("click",()=>{
+  document.querySelector(".formationsLongues").style.display ="none"
+  document.querySelector(".formationsCourtes").style.display = "flex"
+})
+
 
 
 
 //! *********************** Card Formations ***********************
 
-let nomFormation = document.querySelectorAll(".fNom")
-let durForm = document.querySelectorAll(".fDuree")
+//? *********************** Formation longues **********************
 
-//? les conditions 
-let condingCondition = document.querySelectorAll(".condingCondition")
-let mkcondition = document.querySelectorAll(".mkCondition")
-let wbCondition = document.querySelectorAll(".wbCondition")
-let awscondition = document.querySelectorAll(".awsCondition")
-let sfacondition = document.querySelectorAll(".sfaCondition")
 
+let nomFormation = document.querySelectorAll(".fNom");
+let durForm = document.querySelectorAll(".fDuree");
+
+//? les conditions
+let condingCondition = document.querySelectorAll(".condingCondition");
+let mkcondition = document.querySelectorAll(".mkCondition");
+let wbCondition = document.querySelectorAll(".wbCondition");
+let awscondition = document.querySelectorAll(".awsCondition");
+let sfacondition = document.querySelectorAll(".sfaCondition");
 
 Object.keys(DATA.formations.formationsLongues).forEach((element, index) => {
-  nomFormation[index].innerText = element
+  nomFormation[index].innerText = element;
 });
 
-
-function inserer(DATA, cible ,cible2) {
+function inserer(DATA, cible) {
   let formationsLongues = DATA.formations.formationsLongues;
   Object.keys(formationsLongues).forEach((key, index) => {
     let formation = formationsLongues[key];
-    cible[index].innerText = `Nom : ${formation.Nom} \n Durée : ${formation.Duree}\n Descriptif : ${formation.Descriptif} `;
-
-});
-}
-inserer(DATA,durForm)
-
-function lesConditions(formation,cible) {
-  
-  Object.keys(formation).forEach((key, index) => {
-      let conditionValue = formation[key];
-      
-      if (conditionValue === null) {
-          conditionValue = "Pas d'âge requis";
-      }
-        cible[index].innerText += ` ${conditionValue}`;
+    cible[
+      index
+    ].innerText = `Nom : ${formation.Nom} \n Durée : ${formation.Duree}\n Descriptif : ${formation.Descriptif} `;
   });
-  
+}
+inserer(DATA, durForm);
+
+function lesConditions(formation, cible) {
+  Object.keys(formation).forEach((key, index) => {
+    let conditionValue = formation[key];
+
+    if (conditionValue === null) {
+      conditionValue = "Pas d'âge requis";
+    }
+    cible[index].innerText += ` ${conditionValue}`;
+  });
 }
 
-lesConditions(DATA.formations.formationsLongues.codingSchool.condition,condingCondition)
-lesConditions(DATA.formations.formationsLongues.marketingDigital.condition,mkcondition)
-lesConditions(DATA.formations.formationsLongues.webmaster360.condition,wbCondition)
-lesConditions(DATA.formations.formationsLongues.aws.condition,awscondition)
-lesConditions(DATA.formations.formationsLongues.salesforceAdmin.condition,sfacondition)
+lesConditions(
+  DATA.formations.formationsLongues.codingSchool.condition,
+  condingCondition
+);
+lesConditions(
+  DATA.formations.formationsLongues.marketingDigital.condition,
+  mkcondition
+);
+lesConditions(
+  DATA.formations.formationsLongues.webmaster360.condition,
+  wbCondition
+);
+lesConditions(DATA.formations.formationsLongues.aws.condition, awscondition);
+lesConditions(
+  DATA.formations.formationsLongues.salesforceAdmin.condition,
+  sfacondition
+);
+//? ************************** Fin Formations longues ******************
+
+
+//? *************************  Fomations Courtes ********************
+
+let forCourtes = document.querySelector(".formationsCourtes").querySelectorAll("p")
+
+// forCourtes.forEach(element => {
+//   console.log(element);
+// });
+
+Object.values(DATA.formations.formationsCourtes).forEach((element, index) => {
+  forCourtes[index].innerText = ` ${element.Nom} \n ${element.pack}`;
+});
+
 
 //! ******************************* Fin Cards **************************
